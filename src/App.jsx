b5f1d273b593af283fec477472d02adedf6a2e17
Mati Joseph <crucialniccur@gmail.com>
2025-04-12 22:43:30 +0300

    import the Form component in App and render as a child to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./Header";
+import Form from "./Form";
 
 function App() {
   return (
     <>
       <Header />
+      <Form />
     </>
   );
 }
