c5d3302ff2114ba5f0ae4365259626dbd984d585
Mati Joseph <crucialniccur@gmail.com>
2025-04-12 22:36:01 +0300

    import header in app and render it as a child component of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <>
-      <div>app</div>
+      <Header />
     </>
   );
 }
