39f06f33715d767d2d345de6e7674e41a0c7461b
Mati Joseph <crucialniccur@gmail.com>
2025-04-12 22:39:39 +0300

    add a header that renders a h1 heading and a p with some text

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <header>
+        <h1>Expense Tracker</h1>
+        <p>
+          Start taking control of your finances and life.Record ,categorize and
+          analyze your spending.
+        </p>
+      </header>
+    </div>
+  );
 };
 
 export default Header;
