3f95c3b5daa17b505c5e1f270359a93684ed6c68
Mati Joseph <crucialniccur@gmail.com>
2025-04-12 22:46:34 +0300

    add a title and some instructions to the form

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <div>
+      <form>
+        <h2>Add Expense</h2>
+        <p>Enter your expense details below</p>
+      </form>
+    </div>
+  );
 };
 
 export default Form;
