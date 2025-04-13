91491ade35c83c2e8aef5d574e414c484dd321b7
Mati Joseph <crucialniccur@gmail.com>
2025-04-13 06:19:43 +0300

    add some styling

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,39 +3,41 @@ import React from "react";
 const Form = () => {
   return (
     <div>
-      <form>
-        <h2>Add Expense</h2>
-        <p>Enter your expense details below</p>
-        <input type="text" placeholder="Enter expense name"></input>
-        <input type="text" placeholder="Enter expense description"></input>
-        <input type="text" placeholder="Enter expense category"></input>
-        <input type="number" placeholder="Enter amount"></input>
-        <input type="date"></input>
-        <input type="submit" value="Submit" />
-      </form>
-      <div className="tableList">
-        <input type="search" placeholder="Search expenses" />
-        <table>
-          <thead>
-            <tr>
-              <th>Expense</th>
-              <th>Description</th>
-              <th>Category</th>
-              <th>Amount</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Lorem Expense</td>
-              <td>Lorem Description</td>
-              <td>Lorem category</td>
-              <td>Lorem Amount</td>
-              <td>Lorem date</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <section>
+        <form>
+          <h2>Add Expense</h2>
+          <p>Enter your expense details below</p>
+          <input type="text" placeholder="Enter expense name"></input>
+          <input type="text" placeholder="Enter expense description"></input>
+          <input type="text" placeholder="Enter expense category"></input>
+          <input type="number" placeholder="Enter amount"></input>
+          <input type="date"></input>
+          <input type="submit" value="Submit" />
+        </form>
+        <div className="tableList">
+          <input type="search" placeholder="Search expenses" />
+          <table>
+            <thead>
+              <tr>
+                <th>Expense</th>
+                <th>Description</th>
+                <th>Category</th>
+                <th>Amount</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Lorem Expense</td>
+                <td>Lorem Description</td>
+                <td>Lorem category</td>
+                <td>Lorem Amount</td>
+                <td>Lorem date</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
     </div>
   );
 };
